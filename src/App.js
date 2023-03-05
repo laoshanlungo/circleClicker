021a96f31ac26576a9d3ebfdb11028808c76d61d
David <michel.david@gmx.de>
2023-03-05 18:09:09 +0100

    initial commit, setting up basic circle clicker app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import {useState} from 'react';
+import Cookie from './img/cookie.jpg'
 
-function App() {
+
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  const [name, setName] = useState('');
+  const [authentified, setAuthentified] = useState(false);
+  const [upgradeValue, setUpgradeValue] = useState(1)
+  const [buttonDisabled, setButtonDisabled] = useState(true)
+
+  const delay = ms => new Promise(res => setTimeout(res, ms));
+
+
+function logout(){
+  setAuthentified(false)
+  setName('')
+  setUpgradeValue(1)
+  setCounter(0);
+}
+
+async function handleCookieClick(){
+  setCounter(counter+upgradeValue)
+  if(counter===10){
+    setButtonDisabled(false)
+    await delay(10000);
+    setButtonDisabled(true)
+    setUpgradeValue(1)
+  }
+}
+
+
+
+if(authentified){ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{name}`s CookieClicker</h1>
+      <h2>Einfach auf den Cookie rauf</h2>
+      <h3>Anzahl Cookie Klicks {counter}</h3>
+      <br />
+      <img className="cookie" src={Cookie} onClick={() => handleCookieClick()}/> 
+      <br />
+      <h4> Klick den Cookie </h4>
+      <br />
+      <button disabled={buttonDisabled} onClick={() => setUpgradeValue(
+              upgradeValue === 1 ? upgradeValue + 4 : upgradeValue + 5
+            )}> Upgrade Cookie </button>
+      <button onClick={() => logout()}> Log Out </button>
     </div>
   );
 }
 
-export default App;
+  return (
+    <div className="App">
+      <h1>Logge dich erst ein</h1>
+      <input value={name} onChange={(e) => setName(e.target.value)}/>
+      <button onClick={() => setAuthentified(true)}> Log In </button>
+    </div>
+  )
+
+
+ 
+
+
+
+}
